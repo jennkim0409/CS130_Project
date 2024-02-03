041dac504ed782ab739228cf4dc06ff8500d5651
Muskan Shaikh <76590770+Muskan-R-S@users.noreply.github.com>
2024-02-03 12:36:19 -0800

    local DB set-up with docker

--- backend/config.js
+++ backend/config.js
@@ -2,9 +2,13 @@
 export const PORT = 5555;
 
 // Getting the username and password for the DB connection 
-const username = process.env.DB_USERNAME;
+const username = process.env.DB_USER;
+
 const password = process.env.DB_PASSWORD;
+const db_host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+
 
 // Connect to DB
-export const mongodbURL = `mongodb+srv://${username}:${password}@book-pins-db.b5atqpl.mongodb.net/?retryWrites=true&w=majority`
+export const mongodbURL = `mongodb://${username}:${password}@${db_host}:${port}/`
 
