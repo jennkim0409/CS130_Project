048b7ce25e397e77437b501f851289da8a2a0c5e
Jennifer Kim <jennkim0409@g.ucla.edu>
2024-02-09 14:34:12 -0800

    renamed files and folders

---
+++ frontend/src/pages/Boards/Boards.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Boards.css";
+
+function Boards() {
+    return(
+        <div className="Boards">
+            <h1>This is the Home Page for Pinterest Boards</h1>
+        </div>
+    );
+}
+export default Boards;
---
+++ frontend/src/pages/Library/Library.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Library.css";
+import Bookshelf from '../../components/Bookshelf/Bookshelf';
+
+function Library() {
+    return(
+        <div className="Library">
+            <Bookshelf/>
+        </div>
+    );
+}
+export default Library;
