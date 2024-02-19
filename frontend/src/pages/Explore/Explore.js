0cfcf698dc31385752cd1ab1d6324ce2d3b18278
Charlene Kim <hyuncharlenekim@gmail.com>
2024-02-19 01:14:17 -0800

    updated shelf ui based on figma

--- frontend/src/pages/Explore/Explore.js
+++ frontend/src/pages/Explore/Explore.js
@@ -5,6 +5,7 @@ import Bookshelf from '../../components/Bookshelf/Bookshelf';
 function Explore() {
     return(
         <div className="Library">
+            <h2 style={{textAlign: "center"}}>My Shelves</h2>
             <Bookshelf/>
         </div>
     );
