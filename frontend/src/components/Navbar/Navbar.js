7f8f344769f0c376fab7b9baabb4a7a20ff989b9
Jennifer Kim <jennkim0409@g.ucla.edu>
2024-02-20 00:00:32 -0800

    Adjusted UI for login/signup page

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,10 +5,11 @@ import './Navbar.css';
 export default function Navbar() {
     const location = useLocation();
     const navigate = useNavigate();
+    const isLoggedIn = !!localStorage.getItem("user_token");
 
     return <nav className="nav">
         <div className="navbar">
-            <div className="logo" onClick={()=>navigate('/explore')}>
+            <div className="logo" onClick={()=>{isLoggedIn ? navigate('/explore') : navigate('/')}}>
                 <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 0L13.4164 7.64074L19.0534 2.2918L15.7082 9.30583L23.4127 8.2918L16.5836 12L23.4127 15.7082L15.7082 14.6942L19.0534 21.7082L13.4164 16.3593L12 24L10.5836 16.3593L4.94658 21.7082L8.2918 14.6942L0.587322 15.7082L7.41641 12L0.587322 8.2918L8.2918 9.30583L4.94658 2.2918L10.5836 7.64074L12 0Z" fill="#337032"/>
                 </svg>
