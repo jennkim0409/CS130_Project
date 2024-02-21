945bd9cf393bef94e98ad178ee2a40c069207a13
dianaestrada417 <estrada.diana120@gmail.com>
2024-02-21 15:07:04 -0800

    changed controller name

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ import cors from "cors"
 import { PORT, mongodbURL } from "./config.js";
 import loginRouter from "./controllers/login.js"
 import registerRouter from "./controllers/registration.js"
-import updateRouter from "./controllers/update.js";
+import handlebooksRouter from "./controllers/handle_books.js";
+// import fetchBookInfoRouter from "./controllers/fetchBookInfo.js";
 import authenticateMiddleware from "./utils/middleware/auth.js";
 
 const app = express();
@@ -27,7 +28,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/*', authenticateMiddleware);
 //app.use('/api/login/userdata',loginRouter)
-app.use('/api/update', updateRouter);
+app.use('/api/handlebooks', handlebooksRouter);
+// app.use('/api/fetchbook', fetchBookInfoRouter);
 app.use('/auth/register', registerRouter)
 app.use('/auth/login', loginRouter)
 
