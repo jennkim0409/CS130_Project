21ae9190f7a9fae71337983dd3b195a4536246bc
Jennifer Kim <jennkim0409@g.ucla.edu>
2024-02-23 01:44:40 -0800

    Created an option to sign out in accounts page

--- frontend/src/pages/Account/Account.js
+++ frontend/src/pages/Account/Account.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "./Account.css";
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function Account() {
+function Account(props) {
+    const navigate = useNavigate();
+    const signout = () => {
+        if (window.confirm("Are you sure you want to sign out?")) {
+            localStorage.clear();
+            props.signOut();
+            toast.info("Signing you out", {
+                onClose: () => navigate('/'), 
+                autoClose: 2000,
+                pauseOnHover: false,
+            });
+        }
+    }
     return(
-        <div>This will have the user settings.</div>
+        <div className="account">
+            <h2 style={{textAlign: "center"}}>Account</h2>
+            <div className="submit gray" onClick={signout}>Sign Out</div>
+        </div>
     );
 }
 export default Account;
