9387f29417742d1c781e3e41d79a345676e46d6f
Jennifer Kim <jennkim0409@g.ucla.edu>
2024-03-01 00:20:24 -0800

    Added feature for user to enter text in visual boards

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -3,6 +3,10 @@ import './Modal.css';
 import remove from '../../assets/remove.png';
 import upload from '../../assets/cloud-computing.png';
 import _uniqueId from 'lodash/uniqueId';
+import Toggle from 'react-toggle';
+import "react-toggle/style.css"
+import { SketchPicker } from 'react-color';
+import { ChromePicker } from 'react-color'
 
 function upload_img(event, pinDetails, setPinDetails, setShowLabel, setShowModalPin, setImageUploaded) {
     if (event.target.files && event.target.files[0]) {
@@ -72,85 +76,126 @@ function save_pin(pinDetails, boardId, add_pin, id) {
     }
 }
 
+function save_pin_text(pinDetails, boardId, add_pin, id, textColor) {
+    const users_data = {
+        ...pinDetails,
+        board: boardId,
+        title: document.querySelector('#pin_title').value,
+        quote: document.querySelector('#pin_quote').value,
+        text_color: textColor,
+        unique_id: id
+    }
+
+    add_pin(users_data);
+}
+
 function Modal(props) { 
 
     const [pinDetails, setPinDetails] = useState({
         board: '',
         title: '',
         description: '',
+        quote: '',
         img_blob: '',
         pin_size: '',
-        unique_id: ''
+        unique_id: '',
+        text_color: '',
     });
 
     const [showLabel, setShowLabel] = useState(true);
     const [showModalPin, setShowModalPin] = useState(false);
     const [id] = useState(_uniqueId('pin-'));
     const [imageUploaded, setImageUploaded] = useState(false);
+    const [toggleText, setToggleText] = useState(false);
+    const [textColor, setTextColor] = useState('#000');
 
     return (
         <div className="add_pin_modal">
             <div className="add_pin_container">
-                <div className="side" id="left_side">
-                    <div className="section1">
-                        { imageUploaded ? 
-                            <div className="pint_mock_icon_container" onClick={() => remove_image(pinDetails, setPinDetails, setShowLabel, setShowModalPin, setImageUploaded)}>
-                                <img src={remove} alt="remove" className="pint_mock_icon_remove" /> 
-                            </div>
-                            : null
-                    
-                        }
+                <div className="top">
+                    <h5 style={{margin: '5px'}}>Add a Quote Instead</h5>
+                    <label className="toggle-button">
+                        <Toggle
+                            defaultChecked={toggleText}
+                            icons={false}
+                            onChange={() => setToggleText(!toggleText)}
+                        />
+                    </label>
+                </div>
+                { toggleText?
+                    <div className="add_pin_details_text">
+                        <input placeholder="Add a title" type="text" className="new_pin_input" id="pin_title" />
+                        <input placeholder="Enter your Quote or Text" type="text" className="new_pin_input" id="pin_quote" />
+                        <ChromePicker
+                            color={textColor}
+                            onChangeComplete={(color) => setTextColor(color.hex)}
+                        />
+                        <div className="submit" onClick={() => save_pin_text(pinDetails, props.boardId, props.add_pin, id, textColor)}>Save</div>
                     </div>
-                    <div className="section2">
-                        <label htmlFor="upload_img" id="upload_img_label"
-                            style={{
-                                display: showLabel ? 'block' : 'none'
-                            }}
-                        >
-                            <div className="upload_img_container">
-                                <div id="dotted_border">
-                                    <div className="pint_mock_icon_container">
-                                        <img src={upload} alt="upload" className="pint_mock_icon" /> 
-                                    </div>        
-                                    <div>Click to Upload</div>
-                                    <div>Recommendation: Use high quality .jpg less than 20MB</div>    
+                    :
+                    <div className="add_pin_details_image">
+                        <div className="side" id="left_side">
+                            <div className="section1">
+                                { imageUploaded ? 
+                                    <div className="pint_mock_icon_container" onClick={() => remove_image(pinDetails, setPinDetails, setShowLabel, setShowModalPin, setImageUploaded)}>
+                                        <img src={remove} alt="remove" className="pint_mock_icon_remove" /> 
+                                    </div>
+                                    : null
+                            
+                                }
+                            </div>
+                            <div className="section2">
+                                <label htmlFor="upload_img" id="upload_img_label"
+                                    style={{
+                                        display: showLabel ? 'block' : 'none'
+                                    }}
+                                >
+                                    <div className="upload_img_container">
+                                        <div id="dotted_border">
+                                            <div className="pint_mock_icon_container">
+                                                <img src={upload} alt="upload" className="pint_mock_icon" /> 
+                                            </div>        
+                                            <div>Click to Upload</div>
+                                            <div>Recommendation: Use high quality .jpg less than 20MB</div>    
+                                        </div>
+                                    </div>
+                                    <input onChange=
+                                        {event => upload_img(event, pinDetails, setPinDetails, setShowLabel, setShowModalPin, setImageUploaded)}
+                                        type="file" name="upload_img" id="upload_img" value=""
+                                    />
+                                </label>
+                                
+                                <div className="modal_pin"
+                                    style={{
+                                        display: showModalPin ? 'block' : 'none'
+                                    }}
+                                >
+                                    <div className="pin_image">
+                                        <img onLoad={check_size} src={pinDetails.img_blob} alt="pin_image"/>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="side" id="right_side">
+                            <div className="section1">
+                                <div className="select_size">
+                                    <select defaultValue="Select size" name="pin_size" id="pin_size">
+                                        <option value="">Select size</option>
+                                        <option value="small">Small</option>
+                                        <option value="medium">Medium</option>
+                                        <option value="large">Large</option>
+                                    </select>
+                                    <div onClick={() => save_pin(pinDetails, props.boardId, props.add_pin, id)} className="save_pin">Save</div>
                                 </div>
                             </div>
-                            <input onChange=
-                                {event => upload_img(event, pinDetails, setPinDetails, setShowLabel, setShowModalPin, setImageUploaded)}
-                                type="file" name="upload_img" id="upload_img" value=""
-                            />
-                        </label>
-                        
-                        <div className="modal_pin"
-                            style={{
-                                display: showModalPin ? 'block' : 'none'
-                            }}
-                        >
-                            <div className="pin_image">
-                                <img onLoad={check_size} src={pinDetails.img_blob} alt="pin_image"/>
+                            <div className="section2">
+                                <input placeholder="Add a title" type="text" className="new_pin_input" id="pin_title" />
+                                <input placeholder="What is this pin about?" type="text" className="new_pin_input" id="pin_description" />
                             </div>
                         </div>
-
                     </div>
-                </div>
-                <div className="side" id="right_side">
-                    <div className="section1">
-                        <div className="select_size">
-                            <select defaultValue="Select size" name="pin_size" id="pin_size">
-                                <option value="">Select size</option>
-                                <option value="small">Small</option>
-                                <option value="medium">Medium</option>
-                                <option value="large">Large</option>
-                            </select>
-                            <div onClick={() => save_pin(pinDetails, props.boardId, props.add_pin, id)} className="save_pin">Save</div>
-                        </div>
-                    </div>
-                    <div className="section2">
-                        <input placeholder="Add a title" type="text" className="new_pin_input" id="pin_title" />
-                        <input placeholder="What is this pin about?" type="text" className="new_pin_input" id="pin_description" />
-                    </div>
-                </div>
+                }
             </div>
         </div>
     )
---
+++ frontend/src/components/TextSize/TextSize.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState, useRef } from 'react';
+
+function TextSize({ quote, color }) {
+  const quote_ref = useRef(null);
+  const [fontSize, setFontSize] = useState(10);
+
+  const adjustTextSize = () => {
+    if (!quote_ref.current) return;
+
+    const grandparentWidth = quote_ref.current.parentElement.parentElement.offsetWidth;
+    const grandparentHeight = 300; // fixed size of pin
+
+    let minFontSize = 5;
+    let maxFontSize = 100;
+    let currentFontSize = minFontSize;
+    let found = false;
+
+
+    // essentially using binary search to figure out font that would show all text
+    while ((minFontSize <= maxFontSize) && !found) {
+      currentFontSize = Math.floor((minFontSize + maxFontSize) / 2);
+      quote_ref.current.style.fontSize = `${currentFontSize}px`;
+
+      if (quote_ref.current.scrollWidth <= grandparentWidth && quote_ref.current.scrollHeight <= grandparentHeight) {
+        minFontSize = currentFontSize + 1;
+      } 
+      else {
+        maxFontSize = currentFontSize - 1;
+      }
+
+      if ((minFontSize > maxFontSize) && quote_ref.current.scrollHeight <= grandparentHeight) {
+        console.log(quote_ref.current.scrollHeight);
+        found = true;
+        setFontSize(currentFontSize);
+      }
+      // if binary search couldn't get height to be smaller than 300, then subtract by 2
+      else {
+        setFontSize(currentFontSize-2);
+      }
+    }
+  };
+
+  // text size changes on mount
+  useEffect(() => {
+    adjustTextSize();
+  }, [quote]);
+
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <div ref={quote_ref} style={{ fontSize: `${fontSize}px`, color: color}}>
+        {quote}
+      </div>
+    </div>
+  );
+}
+
+export default TextSize;
