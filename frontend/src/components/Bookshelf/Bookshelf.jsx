c0447bd1fb140837432ed3138de8fb10d13bf716
Kaylee Harris <kaylee0129@ucla.edu>
2024-02-29 17:14:57 -0800

    books loaded, little issues with drag and drop

--- frontend/src/components/Bookshelf/Bookshelf.jsx
+++ frontend/src/components/Bookshelf/Bookshelf.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import {
   DndContext,
   closestCenter,
@@ -18,16 +19,50 @@ import {
 import {Grid} from './Grid';
 import {SortablePhoto} from './SortablePhoto';
 import {Photo} from './Photo';
-import reading from './reading.json';
-import recommended from './recommended.json';
 
 const Bookshelf = () => {
   // items imported from .json (links to images)
-  // hash table into readingList and recommendedList
+  // hash table into readingList and finishedList
   const [items, setItems] = useState({
-    readingList: reading,
-    recommendedList: recommended,
-  })
+    readingList: [],
+    finishedList: [],
+  });
+
+  // runs when component mounts
+  useEffect(() => {
+    const fetchBookshelfData = async () => {
+      try {
+        const shelves = ["current", "finished"];
+        // get books for each shelf
+        const promises = shelves.map(async (shelfName) => {
+          const response = await axios.get('http://localhost:5555/api/handlebooks/getBooks/', {
+            params: { userId: localStorage.getItem("user_id").replace(/"/g, ''), type: shelfName },
+            headers: { Authorization: localStorage.getItem("user_token") }
+          });
+          return { [shelfName]: response.data };
+        });
+
+        // wait for data for all shelves to be fetched
+        const results = await Promise.all(promises);
+
+        // format data to match other code in this file
+        const itemsData = {};
+        results.forEach((result) => {
+          const shelfName = Object.keys(result)[0];
+          const itemsDataShelfName = shelfName === "current" ? "readingList" : "finishedList";
+          itemsData[itemsDataShelfName] = result[shelfName];
+        });
+
+        // populate each bookshelf with book objects
+        setItems(itemsData);
+        console.log(itemsData)
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    };
+
+    fetchBookshelfData();
+  }, []);
 
   // activeId denotes what component is currently interacted with
   // setActiveId handles updating this ID
@@ -58,18 +93,18 @@ const Bookshelf = () => {
         <SortableContext items={items.readingList} strategy={rectSortingStrategy}>
         {/* can adjust the # of columns, but update .css accordingly */}
           <Grid title='Interested' columns={8}>
-            {items.readingList.map((url, index) => (
-              <SortablePhoto key={url} url={url} index={index} />
+            {items.readingList.map((book, index) => (
+              <SortablePhoto key={book.cover} url={book.cover} index={index} />
             ))}
           </Grid>
         </SortableContext>
       </div>
       
       <div>
-        <SortableContext items={items.recommendedList} strategy={rectSortingStrategy}>
+        <SortableContext items={items.finishedList} strategy={rectSortingStrategy}>
           <Grid title='Finished' columns={8}>
-            {items.recommendedList.map((url, index) => (
-              <SortablePhoto key={url} url={url} index={index} />
+            {items.finishedList.map((book, index) => (
+              <SortablePhoto key={book.cover} url={book.cover} index={index} />
             ))}
           </Grid>
         </SortableContext>
@@ -100,7 +135,6 @@ const Bookshelf = () => {
   should be able to hop between position and containers
   not super necessary to understand logic!  */
   function handleDragOver(event) {
-
     // active is the dragging component
     // over is the position it is dragging over
     const { active, over, draggingRect } = event;
@@ -124,8 +158,8 @@ const Bookshelf = () => {
       const overItems = prev[overContainer];
 
       // find the indexes for the items
-      const activeIndex = activeItems.indexOf(id);
-      const overIndex = overItems.indexOf(overId);
+      const activeIndex = activeItems.findIndex(book => book.cover === id);
+      const overIndex = overItems.findIndex(book => book.cover === overId);
 
       let newIndex;
       if (overId in prev) {
@@ -145,7 +179,7 @@ const Bookshelf = () => {
       return {
         ...prev,
         [activeContainer]: [
-          ...prev[activeContainer].filter((item) => item !== active.id)
+          ...prev[activeContainer].filter((book) => book.cover !== active.id)
         ],
         [overContainer]: [
           ...prev[overContainer].slice(0, newIndex),
@@ -166,6 +200,10 @@ const Bookshelf = () => {
     // find the bookshelf containers that they are in
     const activeContainer = findContainer(id);
     const overContainer = findContainer(overId);
+    console.log("active cont")
+    console.log(activeContainer)
+    console.log("over cont")
+    console.log(overContainer)
     // if there is no active, over, or if they are do not collide
     // (i.e. no interaction being made, simply return)
     if (
@@ -174,18 +212,53 @@ const Bookshelf = () => {
       activeContainer !== overContainer
     ) {
       return;
-    }
+    }   
 
-    const activeIndex = items[activeContainer].indexOf(active.id);
-    const overIndex = items[overContainer].indexOf(overId);
+    // it's saying they are the same because handleDragOver is assigning the item
+    // to the new shelf BEFORE i drop it in ?
+
+    // TO DO: the code below needs to only run if the containers are the same.
+    // currently it runs regardless of that
+ 
+    // for item that will be displaced (moved to the right)
+    // after insertion of new element
+    // active index = initial index of this item
+    // over index = resulting index of this item (always 1 more)
+    // UNLESS it is being moved within same shelf, then it works fine
+
+    console.log("active id")
+    console.log(active.id)
+    console.log("over id")
+    console.log(overId)
+    const activeIndex = items[activeContainer].findIndex(book => book.cover === active.id);
+    console.log("active index drop end")
+    console.log(activeIndex)
+    const overIndex = items[overContainer].findIndex(book => book.cover === overId);
+    console.log("over index drop end")
+    console.log(overIndex)
+
+    // it's moving the wrong item
+    // it's moving the item we JUST INSERTED one forward
+    // but in reality it should be moving the one that was just there, forward
+
+    // i.e. active ID object is getting moved forward instead of over ID object
 
     // if the draggable is dragged into a new position
+    // if we are shifting in among the same shelf we want this: 
     if (activeIndex !== overIndex) {
       setItems((items) => ({
         ...items,
         [overContainer]: arrayMove(items[overContainer], activeIndex, overIndex)
       }));
     }
+
+    // if are shifting between shelves we want this: 
+    // if (activeIndex !== overIndex) {
+    //   setItems((items) => ({
+    //     ...items,
+    //     [overContainer]: arrayMove(items[overContainer], activeIndex+1, overIndex)
+    //   }));
+    // }
 
     // after exchange is made, set active ID to null
     setActiveId(null);
@@ -201,9 +274,12 @@ const Bookshelf = () => {
     if (id in items) {
       return id;
     }
-    return Object.keys(items).find((key) => items[key].includes(id));
+
+    return Object.keys(items).find((shelfName) => {
+      const shelf = items[shelfName];
+      return shelf.some(book => book.cover === id);
+    });
   }
-  
 };
 
 export default Bookshelf;
