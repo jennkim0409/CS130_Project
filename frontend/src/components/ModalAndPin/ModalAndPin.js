16fecb7d56f57121fc2c754e1ec987a1f32aa4ed
Jennifer Kim <jennkim0409@g.ucla.edu>
2024-03-03 18:01:20 -0800

    Wrote frontend code to make existing pins clickable for viewing and/or updating

--- frontend/src/components/ModalAndPin/ModalAndPin.js
+++ frontend/src/components/ModalAndPin/ModalAndPin.js
@@ -9,7 +9,8 @@ class ModalAndPin extends React.Component {
         super(props);
         this.state={
             pins: [],
-            show_modal: false
+            show_modal: false,
+            editPinDetails: null,
         };
     }
 
@@ -22,14 +23,37 @@ class ModalAndPin extends React.Component {
             // key is an ordering attribute of this pin
             // we use the length of the state pins since this keeps increasing with every new pin
             new_pins.push(
-                <Pin pinDetails={pinDetails} key={pinDetails.ordering_id} pinId={pinDetails.ordering_id} removePin={this.remove_pin}/>
+                <Pin pinDetails={pinDetails} key={pinDetails.ordering_id} pinId={pinDetails.ordering_id} removePin={this.remove_pin} editPin={this.editPin}/>
             )
             console.log(new_pins);
             return {
                 pins: new_pins,
-                show_modal: false
+                show_modal: false,
+                editPinDetails: null,
             }
         })
+    }
+
+    // function to account for updating a pin
+    change_pin = pinDetails => {
+        this.setState(_state => {
+            // map through the existing pins to find the one to update
+            const updated = _state.pins.map(pinElement => {
+                if (pinElement.props.pinId === pinDetails.ordering_id) {
+                    // return new pin with updated details
+                    return <Pin pinDetails={pinDetails} key={pinDetails.ordering_id} pinId={pinDetails.ordering_id} removePin={this.remove_pin} editPin={this.editPin}/>;
+                } else {
+                    return pinElement;
+                }
+            });
+
+            return {
+                ..._state,
+                pins: updated,
+                show_modal: false, 
+                editPinDetails: null,
+            };
+        });
     }
 
     remove_pin = (orderingId) => {
@@ -37,6 +61,16 @@ class ModalAndPin extends React.Component {
             pins: prevState.pins.filter(pinElement => pinElement.props.pinDetails.ordering_id !== orderingId)
         }));
     }
+
+    // function used when pin is clicked
+    // we know that we want to edit/view this pin, so update to show modal and also set in editing view
+    editPin = (pinDetails) => {
+        // Set state with pinDetails to edit and make modal visible
+        this.setState({
+            editPinDetails: pinDetails,
+            show_modal: true,
+        });
+    };
 
     render() {
         return (
@@ -52,13 +86,13 @@ class ModalAndPin extends React.Component {
                 </div>
 
                 <div onClick={
-                    event => event.target.className === "add_pin_modal" ? this.setState({show_modal: false}) : null
+                    event => event.target.className === "add_pin_modal" ? this.setState({show_modal: false, editPinDetails: null}) : null
                     }
                     className="add_pin_modal_container"
                 >
                     {
                         this.state.show_modal ? 
-                        <Modal add_pin={this.add_pin}/> : null
+                        <Modal add_pin={this.add_pin} editPinDetails={this.state.editPinDetails} change_pin={this.change_pin}/> : null
                     }
                 </div>
             </div>
