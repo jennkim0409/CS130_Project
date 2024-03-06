d90aa3c975e891c68356d9c7160ebdab600d98df
Jennifer Kim <jennkim0409@g.ucla.edu>
2024-03-06 01:15:17 -0800

    create button and toggle for board discovery/share

--- frontend/src/pages/Board/Board.js
+++ frontend/src/pages/Board/Board.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Board.css';
 import { useParams, useNavigate } from 'react-router-dom';
 import ModalAndPin from '../../components/ModalAndPin/ModalAndPin';
-import { ReactComponent as BackIcon } from '../../assets/back.svg'; 
+import { ReactComponent as BackIcon } from '../../assets/back.svg';
+import Toggle from 'react-toggle'
 
 const Board = () => {
   const navigate = useNavigate();
   const { boardId } = useParams(); // This hooks into the router and gives you the dynamic part of the URL
+  const [togglePublic, setTogglePublic] = useState(true);
+  
 
-  console.log(boardId);
-  // Here, you can fetch data based on boardId, or import a layout component and pass boardId to it
   return (
     <div className='board'>
       <div onClick={() => navigate('/boards')} className="go_back">
         <BackIcon className="back_svg"/>
         <h3>Go Back</h3>
-      </div> 
+      </div>
+      <div className="discover">
+        Discover
+      </div>
       <div className="boardInfo">
         <h1>Board: {boardId}</h1>
-        {/* 
-        keep in mind that we'll be using boardId, which is the unique ID, to fetch
-        information about the book including title, author, book cover 
-        => this area should be replaced with book cover, title, author
-        */}
+      </div>
+      <div className="privacy">
+      <label>
+        <h4 style={{margin: '5px'}}>Make Public?</h4>
+          <Toggle
+            defaultChecked={togglePublic}
+            onChange={() => setTogglePublic(!!togglePublic)}
+          />
+      </label>
       </div>
       <div className="modal">
-        {/* Render your layout and data based on boardId */}
         <ModalAndPin/>
       </div>
     </div>
