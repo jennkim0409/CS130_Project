50fa88817f4e6402d7c7ccb4d050838ac81b7ba5
Charlene Kim <hyuncharlenekim@gmail.com>
2024-03-05 22:29:19 -0800

    genre selections in recommendations ui

--- frontend/src/pages/Explore/Explore.js
+++ frontend/src/pages/Explore/Explore.js
@@ -1,13 +1,21 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Explore.css";
 import Recommendations from '../../components/Recommendations/Recommendations';
+import GenrePreferences from '../../components/GenrePreferences/GenrePreferences';
 
 function Explore() {
+    const [genrePreferences, setGenrePreferences] = useState([]);
+
     return(
         <div className="Explore">
-            <h2 style={{textAlign: "center"}}>Explore</h2>
+            <h2 style={{ textAlign: "center" }}>Explore</h2>
+            <div style={{ display: "flex", flexDirection: "column", paddingLeft: "30%" }}>
+                <GenrePreferences return_genres={setGenrePreferences} />
+            </div>
+            <br/><br/>
             <Recommendations/>
         </div>
+
     );
 }
 export default Explore;
