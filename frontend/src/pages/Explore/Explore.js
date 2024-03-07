e2a96f7af0dd2d6574ebfa5b2c66eea09deabf97
Charlene Kim <hyuncharlenekim@gmail.com>
2024-03-07 15:25:58 -0800

    optional message in Explore

--- frontend/src/pages/Explore/Explore.js
+++ frontend/src/pages/Explore/Explore.js
@@ -9,8 +9,10 @@ function Explore() {
     return(
         <div className="Explore">
             <h2 style={{ textAlign: "center" }}>Explore</h2>
+            
             <div style={{ display: "flex", flexDirection: "column", paddingLeft: "30%" }}>
                 <GenrePreferences return_genres={setGenrePreferences} />
+                <h5 style={{ paddingRight: "40%" }}>Optional: If you do not select from this list, we will work with your previous genre preferences!</h5>
             </div>
             <br/><br/>
             <Recommendations/>
