4ffb3172574770a1d910a7c74566a8b666d94a39
Charlene Kim <hyuncharlenekim@gmail.com>
2024-03-09 23:08:36 -0800

    account settings, genre preferences and layout

--- frontend/src/pages/Account/Account.js
+++ frontend/src/pages/Account/Account.js
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import GenrePreferences from '../../components/GenrePreferences/GenrePreferences';
+
 
 function Account(props) {
     const navigate = useNavigate();
@@ -12,6 +14,7 @@ function Account(props) {
     const [currentPassword, setCurrentPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [newPassword2, setNewPassword2] = useState('');
+    const [genrePreferences, setGenrePreferences] = useState([]);
 
     useEffect(() => {
         const storedName = localStorage.getItem('user_name');
@@ -19,6 +22,49 @@ function Account(props) {
             setName(storedName);
         }
     }, []);
+
+    function saveGenrePrefs() {
+        // save genre preferences in backend
+        const genrePrefsArray = genrePreferences.map(pref => pref.value);
+        const path = 'http://localhost:5555/api/user/set/' + localStorage.getItem("user_id").replace(/"/g, ''); // gets rid of double quotes in user_id
+        axios.patch(path,
+            { genrePrefs: genrePrefsArray },
+            {
+              headers: {
+                Authorization: localStorage.getItem("user_token")
+              }
+            }
+        )
+        .then(response => {
+            console.log("Genre preferences saved successfully: ", response.data);
+        })
+        .catch(error => {
+            console.error("Error saving genre preferences: ", error);
+            const error_message = error.response.data.message;
+            toast.error(error_message);
+            return;
+        });
+
+        // update recommended shelf to be empty
+        // when we reload the page, new recommendations will be fetched
+        axios.post('http://localhost:5555/api/handlebooks/clearBookshelf/', 
+            { userId: localStorage.getItem("user_id").replace(/"/g, ''), bookshelfType: "recommended" }, 
+            {
+            headers: {
+                Authorization: localStorage.getItem("user_token")
+            }
+        })
+        .then(response => {
+            console.log("Old recommended bookshelf cleared: ", response.data);
+        })
+        .catch(error => {
+            console.error("Error clearing old recommended bookshelf: ", error.response);
+            return;
+        });
+
+        toast.success("Genre preferences saved!");
+        toast.info("Reload the page to get new book recommendations.")
+    }
 
     const signout = () => {
         if (window.confirm("Are you sure you want to sign out?")) {
@@ -101,52 +147,76 @@ function Account(props) {
     };
 
     return(
-        <div className="account">
-            <h2 style={{textAlign: "center"}}>Account</h2>
-            <h2 style={{margin: '0px'}}>Welcome Back, {name}!</h2>
-            <div className="update-name">
-                <h3 style={{textAlign: "center", paddingTop: '15px'}}>Update Name</h3>
-                <div className='account-input'>
-                    <h4>Name</h4>
-                    <input 
-                    type='text'
-                    value={nameInput}
-                    onChange={(e) => setNameInput(e.target.value)}
-                    />
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <div style={{width: "60vw"}}>
+                <h2 style={{textAlign: "center"}}>Account</h2>
+                <h2 style={{margin: '0px', textAlign: "center"}}>Welcome Back, {name}!</h2>
+                <br/><br/>
+                <div style={{ display: 'flex' }}>
+                    <div className="account" style={{width: '50%'}}>
+                        <div className="update-name">
+                            <h3 style={{textAlign: "center"}}>Update Name</h3>
+                            <div className='account-input'>
+                                <h4 style={{marginBottom: '5px'}}>Name</h4>
+                                <input 
+                                type='text'
+                                value={nameInput}
+                                onChange={(e) => setNameInput(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div className="update-password">
+                            <h3 style={{textAlign: "center", paddingTop: '15px'}}>Update Password</h3>
+                            <div className='account-input'>
+                                <h4 style={{marginBottom: '5px'}}>Current Password</h4>
+                                <input 
+                                type='password'
+                                value={currentPassword}
+                                onChange={(e) => setCurrentPassword(e.target.value)}
+                                />
+                            </div>
+                            <div className='account-input'>
+                                <h4 style={{marginBottom: '5px'}}>New Password</h4>
+                                <input 
+                                type='password'
+                                value={newPassword}
+                                onChange={(e) => setNewPassword(e.target.value)}
+                                />
+                            </div>
+                            <div className='account-input'>
+                                <h4 style={{marginBottom: '5px'}}>Re-enter New Password</h4>
+                                <input 
+                                type='password'
+                                value={newPassword2}
+                                onChange={(e) => setNewPassword2(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div className="account-buttons">
+                            <div className="submit" onClick={update} >Update</div>
+                        </div>
+                    </div>
+
+                    <div style={{width: '50%'}}>
+                        <h3 style={{ textAlign: "center" }}>Genre Preferences</h3>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                            <GenrePreferences return_genres={setGenrePreferences} />
+                            <h5 style={{ width: "30vw", minWidth: '300px' }}>Optional: Select above if you want to add additional genre preferences. Otherwise, we will use your previous history of them!</h5>
+                            <div style={{ display: "flex" }}>
+                                <div className="submit" onClick={saveGenrePrefs}>Save</div>
+                            </div>
+                        </div>
+                        <h3 style={{ textAlign: "center", paddingTop: '15px' }}>Manage Account</h3>
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <div className="submit gray" onClick={signout}>Sign Out</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="update-password">
-                <h3 style={{textAlign: "center", paddingTop: '15px'}}>Update Password</h3>
-                <div className='account-input'>
-                    <h4>Current Password</h4>
-                    <input 
-                    type='password'
-                    value={currentPassword}
-                    onChange={(e) => setCurrentPassword(e.target.value)}
-                    />
-                </div>
-                <div className='account-input'>
-                    <h4 >New Password</h4>
-                    <input 
-                    type='password'
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                    />
-                </div>
-                <div className='account-input'>
-                    <h4>Re-enter New Password</h4>
-                    <input 
-                    type='password'
-                    value={newPassword2}
-                    onChange={(e) => setNewPassword2(e.target.value)}
-                    />
-                </div>
-            </div>
-            <div className="account-buttons">
-                <div className="submit" onClick={update} >Update</div>
-                <div className="submit gray" onClick={signout}>Sign Out</div>
             </div>
         </div>
+        
+        
+        
     );
 }
 export default Account;
--- frontend/src/pages/Explore/Explore.js
+++ frontend/src/pages/Explore/Explore.js
@@ -1,71 +1,15 @@
 import { React, useState } from "react";
 import "./Explore.css";
 import Recommendations from '../../components/Recommendations/Recommendations';
-import GenrePreferences from '../../components/GenrePreferences/GenrePreferences';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 
 function Explore() {
-    const [genrePreferences, setGenrePreferences] = useState([]);
-
-    function saveGenrePrefs() {
-        // save genre preferences in backend
-        const genrePrefsArray = genrePreferences.map(pref => pref.value);
-        const path = 'http://localhost:5555/api/user/set/' + localStorage.getItem("user_id").replace(/"/g, ''); // gets rid of double quotes in user_id
-        axios.patch(path,
-            { genrePrefs: genrePrefsArray },
-            {
-              headers: {
-                Authorization: localStorage.getItem("user_token")
-              }
-            }
-        )
-        .then(response => {
-            console.log("Genre preferences saved successfully: ", response.data);
-        })
-        .catch(error => {
-            console.error("Error saving genre preferences: ", error);
-            const error_message = error.response.data.message;
-            toast.error(error_message);
-            return;
-        });
-
-        // update recommended shelf to be empty
-        // when we reload the page, new recommendations will be fetched
-        axios.post('http://localhost:5555/api/handlebooks/clearBookshelf/', 
-            { userId: localStorage.getItem("user_id").replace(/"/g, ''), bookshelfType: "recommended" }, 
-            {
-            headers: {
-                Authorization: localStorage.getItem("user_token")
-            }
-        })
-        .then(response => {
-            console.log("Old recommended bookshelf cleared: ", response.data);
-        })
-        .catch(error => {
-            console.error("Error clearing old recommended bookshelf: ", error.response);
-            return;
-        });
-
-        toast.success("Genre preferences saved!");
-        toast.info("Reload the page to get new book recommendations.")
-    }
 
     return(
         <div className="Explore">
             <h2 style={{ textAlign: "center" }}>Explore</h2>    
-            <h4 style={{ textAlign: "center" }}>Genre Preferences</h4>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-                <GenrePreferences return_genres={setGenrePreferences} />
-                <h5 style={{ width: "30vw", minWidth: '300px' }}>Optional: Select above if you want to add additional genre preferences. Otherwise, we will use your previous history of them!</h5>
-                <div style={{ display: "flex" }}>
-                    <div className="submit gray" style={{ width: "10vw", minWidth: "100px" }} onClick={saveGenrePrefs}>Save</div>
-                </div>
-            </div>
-            
-
-            <br/><br/>
             <Recommendations/>
             <ToastContainer/>
         </div>
